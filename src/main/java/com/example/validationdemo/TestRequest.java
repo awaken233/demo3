8cabc1bdae7c8fb77257cabb30bbef0eed30abc9
awaken233 <1083954992@qq.com>
2026-07-06 11:14:53 +0800

    Add request DTO with validation constraints

---
+++ src/main/java/com/example/validationdemo/TestRequest.java
@@ -0,0 +1,29 @@
+package com.example.validationdemo;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public class TestRequest {
+
+    @NotNull
+    private Integer id;
+
+    @NotBlank
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
